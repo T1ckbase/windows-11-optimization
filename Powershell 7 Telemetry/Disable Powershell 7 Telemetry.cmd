169e533dca5ac5247120f376af0f464261f7004a
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2025-08-14 02:32:12 +0800

    first commit

---
+++ Powershell 7 Telemetry/Disable Powershell 7 Telemetry.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+if not "%~1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%~0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+
+powershell -NoP -NonI "[Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')"
+
+
+echo.
+echo Finished.
+echo Press any key to exit...
+pause > nul
+exit /b
---
+++ Telemetry/Disable Telemetry.cmd
@@ -0,0 +1,52 @@
+@echo off
+setlocal
+if not "%~1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%~0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+
+:: services
+sc config DiagTrack start=disabled
+
+:: scheduled tasks
+schtasks /change /tn "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /disable
+schtasks /change /tn "Microsoft\Windows\Application Experience\ProgramDataUpdater" /disable
+schtasks /change /tn "Microsoft\Windows\Autochk\Proxy" /disable
+schtasks /change /tn "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /disable
+schtasks /change /tn "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /disable
+schtasks /change /tn "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable
+schtasks /change /tn "Microsoft\Windows\Feedback\Siuf\DmClient" /disable
+schtasks /change /tn "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" /disable
+schtasks /change /tn "Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable
+schtasks /change /tn "Microsoft\Windows\Application Experience\MareBackup" /disable
+schtasks /change /tn "Microsoft\Windows\Application Experience\StartupAppTask" /disable
+schtasks /change /tn "Microsoft\Windows\Application Experience\PcaPatchDbTask" /disable
+schtasks /change /tn "Microsoft\Windows\Maps\MapsUpdateTask" /disable
+
+:: registries
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "ContentDeliveryAllowed" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "OemPreInstalledAppsEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "PreInstalledAppsEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "PreInstalledAppsEverEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SilentInstalledAppsEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338387Enabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338388Enabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-353698Enabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SystemPaneSuggestionsEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d "0" /f
+:: reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d "1" /f
+:: reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /t REG_DWORD /d "1" /f
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableTailoredExperiencesWithDiagnosticData" /t REG_DWORD /d "1" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /v "DisabledByGroupPolicy" /t REG_DWORD /d "1" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Disabled" /t REG_DWORD /d "1" /f
+:: reg add "HKLM\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v "fAllowToGetHelp" /t REG_DWORD /d "0" /f
+
+
+echo.
+echo Finished, please reboot your device.
+pause > nul
+exit /b
